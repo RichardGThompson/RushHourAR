afc1951e4fc2e826f1d1e27b27dfb67ac3ab7433
Graham Thompson <38708439+TheOtowngman@users.noreply.github.com>
2019-03-12 18:08:04 -0400

    FirstCommit

---
+++ Assets/Scripts/CarDetection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarDetection : MonoBehaviour
+{
+    private float raycastDistance;
+    public bool raycastResult(float direction, float carSize)
+    {
+        raycastDistance = carSize;
+        RaycastHit hit;
+
+        Ray raycast = new Ray(this.transform.position, direction * transform.right);
+        if(Physics.Raycast(raycast, out hit, raycastDistance))
+        {
+            if(hit.transform.gameObject.tag == "Goal")
+            {
+                return false;
+            }
+            else
+            {
+                Debug.Log("Collision Detected!");
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Scripts/DPadMovement.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DPadMovement : MonoBehaviour
+{
+    private GameObject selectedObject, previousObject;
+    private Button upButton, downButton, rightButton, leftButton, undoButton;
+
+    private float carSize;
+    private bool undoneAlready, firstTurn = false;
+    private int currentObjectMoveCount, previousAction, previousMoveCount, moveCount = 0;
+
+
+    void ButtonPressed(int action)
+    {
+        if(action == 4)
+        {
+            Debug.Log("Undo Pressed!");
+            if(undoneAlready == false && currentObjectMoveCount != 0)
+            {
+                switch (previousAction)
+                {
+                    case 0:
+                        if(selectedObject != previousObject)
+                        {
+                            selectedObject.transform.Translate(-1, 0, 0);
+                        }
+                        else
+                        {
+                            previousObject.transform.Translate(-1, 0, 0);
+                        }
+                        undoneAlready = true;
+                        previousMoveCount = moveCount;
+                        moveCount--;
+                        break;
+                    case 1:
+                        if (selectedObject != previousObject)
+                        {
+                            selectedObject.transform.Translate(1, 0, 0);
+                        }
+                        else
+                        {
+                            previousObject.transform.Translate(1, 0, 0);
+                        }
+                        undoneAlready = true;
+                        previousMoveCount = moveCount;
+                        moveCount--;
+                        break;
+                }
+            }
+        }
+
+        if(selectedObject.tag == "Moveable2" || selectedObject.tag == "Moveable3")
+        {
+            if (selectedObject.tag == "Moveable2")
+            {
+                carSize = 1.01f;
+            }
+            else if(selectedObject.tag == "Moveable3")
+            {
+                carSize = 1.51f;
+            }
+            switch (action)
+            {
+                case 0:
+                    if (selectedObject.transform.rotation.eulerAngles.y == 0)
+                    {
+                        //Move it up if the object is facing up-down.
+                        if(selectedObject.GetComponent<CarDetection>().raycastResult(1.0f, carSize) == false)
+                        {
+                            selectedObject.transform.Translate(1, 0, 0);
+                        }
+                        currentObjectMoveCount++;
+                        previousAction = 0;
+                    }
+                    break;
+                case 1:
+                    if (selectedObject.transform.rotation.eulerAngles.y == 0)
+                    {
+                        //Move it down if the object is facing up-down.
+                        if (selectedObject.GetComponent<CarDetection>().raycastResult(-1.0f, carSize) == false)
+                        {
+                            selectedObject.transform.Translate(-1, 0, 0);
+                        }
+                        currentObjectMoveCount++;
+                        previousAction = 1;
+                    }
+                    break;
+                case 2:
+                    if (selectedObject.transform.rotation.eulerAngles.y == 90 || selectedObject.transform.rotation.eulerAngles.y == 270)
+                    {
+                        //Move it right if the object is facing left-right.
+                        if (selectedObject.GetComponent<CarDetection>().raycastResult(1.0f, carSize) == false)
+                        {
+                            selectedObject.transform.Translate(1, 0, 0);
+                        }
+                        currentObjectMoveCount++;
+                        previousAction = 0;
+                    }
+                    break;
+                case 3:
+                    if (selectedObject.transform.rotation.eulerAngles.y == 90 || selectedObject.transform.rotation.eulerAngles.y == 270)
+                    {
+                        //Move it left if the object is facing left-right.
+                        if (selectedObject.GetComponent<CarDetection>().raycastResult(-1.0f, carSize) == false)
+                        {
+                            selectedObject.transform.Translate(-1, 0, 0);
+                        }
+                        currentObjectMoveCount++;
+                        previousAction = 1;
+                    }
+                    break;
+            }
+        }   
+    }
+    
+    void selectCar()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began && UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject() == false)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (selectedObject != hit.transform.gameObject)
+                    {
+                        undoneAlready = false;
+                        currentObjectMoveCount = 0;
+                    }
+                    switch (firstTurn)
+                    {
+                        case false:
+                            previousObject = selectedObject;
+                            selectedObject = hit.transform.gameObject;
+                            break;
+                        case true:
+                            selectedObject = hit.transform.gameObject;
+                            previousObject = selectedObject;
+                            firstTurn = false;
+                            break;
+                    }
+                    Debug.Log(selectedObject.name);
+                    Debug.Log(previousObject.name);
+                }
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        upButton = GameObject.Find("UpButton").GetComponent<Button>();
+        downButton = GameObject.Find("DownButton").GetComponent<Button>();
+        rightButton = GameObject.Find("RightButton").GetComponent<Button>();
+        leftButton = GameObject.Find("LeftButton").GetComponent<Button>();
+        undoButton = GameObject.Find("UndoButton").GetComponent<Button>();
+
+        upButton.onClick.AddListener(() => ButtonPressed(0));
+        downButton.onClick.AddListener(() => ButtonPressed(1));
+        rightButton.onClick.AddListener(() => ButtonPressed(2));
+        leftButton.onClick.AddListener(() => ButtonPressed(3));
+        undoButton.onClick.AddListener(() => ButtonPressed(4));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        selectCar();
+    }
+}
