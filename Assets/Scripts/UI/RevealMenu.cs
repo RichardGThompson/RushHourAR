e04a9fd2db61a55c00726b053b8a78b5da02eea0
blairbobaggins <48497324+blairbobaggins@users.noreply.github.com>
2019-03-13 13:29:22 -0400

    Added prototype level selector

---
+++ Assets/Scripts/UI/RevealMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RevealMenu : MonoBehaviour
+{
+    public RectTransform menu;
+    public RectTransform menubutton;
+    public Animator menuanimation;
+    bool show = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(show)
+        {
+            menu.anchoredPosition = menubutton.anchoredPosition * Time.deltaTime;
+        }
+    }
+    public void ShowMenu()
+    {
+        menuanimation.SetTrigger("RevealMenu");
+    }
+    public void RetractMenu()
+    {
+
+        menuanimation.SetTrigger("RetractMenu");
+    }
+}
