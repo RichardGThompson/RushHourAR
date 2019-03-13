77ce2db5c9d9bb1bc69bba835753913373cada5e
Graham Thompson <38708439+TheOtowngman@users.noreply.github.com>
2019-03-13 14:04:15 -0400

    Camera switching working.

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -6,16 +6,39 @@ using UnityEngine.UI;
 public class CameraSwitch : MonoBehaviour
 {
     private Button cameraButton;
-    private Camera ARCamera, TopDownCamera;
+    private Camera arCamera, topDownCamera;
+    private bool arEnabled = false;
+    
     void changeCamera()
     {
-
+        if(arCamera.enabled == true)
+        {
+            Debug.Log("Switch To Top-Down Camera.");
+            arCamera.enabled = false;
+            topDownCamera.enabled = true;
+            arCamera.tag = "3DCamera";
+            topDownCamera.tag = "MainCamera";
+        }
+        else if (topDownCamera.enabled == true)
+        {
+            Debug.Log("Switch to AR Camera.");
+            arCamera.enabled = true;
+            topDownCamera.enabled = false;
+            arCamera.tag = "MainCamera";
+            topDownCamera.tag = "3DCamera";
+        }
     }
+
+
 
     private void Start()
     {
         cameraButton = GameObject.Find("CameraButton").GetComponent<Button>();
-        ARCamera = GameObject.Find("")
+        arCamera = GameObject.Find("ARCamera").GetComponent<Camera>();
+        topDownCamera = GameObject.Find("TopDownCamera").GetComponent<Camera>();
+        Debug.Log("Init.");
+        arCamera.enabled = true;
+        topDownCamera.enabled = false;
         cameraButton.onClick.AddListener(() => changeCamera());
     }
 }
