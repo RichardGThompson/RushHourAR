dd85c908a63a90afc4cefa1d55792a1a350d3fb4
blairbobaggins <48497324+blairbobaggins@users.noreply.github.com>
2019-03-27 11:29:17 -0400

    Implementing main menu.
    Not fully complete. Layout and buttons implemented

---
+++ Assets/Scripts/UI/MainMenu/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject greenbutton;
+    public GameObject yellowbutton;
+    public GameObject redbutton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GreenButton()
+    {
+
+    }
+    public void YellowButton()
+    {
+
+    }
+    public void RedButton()
+    {
+
+    }
+}
