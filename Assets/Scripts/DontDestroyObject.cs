ce712bd12b5a340f607c8d96dd45dbbbdf372686
blairbobaggins <48497324+blairbobaggins@users.noreply.github.com>
2019-03-27 16:17:55 -0400

    Settings being implemented.
    Not fully complete yet.

--- Assets/Scripts/DontDestroyObject.cs
+++ Assets/Scripts/DontDestroyObject.cs
@@ -8,7 +8,7 @@ public class DontDestroyObject : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        GameObject[] dontDestroyArray = GameObject.FindGameObjectsWithTag("PositionTracker");
+        GameObject[] dontDestroyArray = GameObject.FindGameObjectsWithTag(gameObject.tag);
         
         if(dontDestroyArray.Length > 1)
         {
