23c4d62b4f6b675506a7dd0bea7d66c71474c986
HUB Zach <dzachvannoppen@gmail.com>
2019-04-02 21:57:03 -0400

    changes march 2

---
+++ Assets/Scripts/SoundEffects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffects : MonoBehaviour
+{
+    public AudioClip aClip;
+    public AudioSource aSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+        aSource.clip = aClip;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        aSource.Play();
+    }
+}
